360d733fcd42372e2c0a9b7aa2c021990f0e2ee0
Ion Sobol <ionsobol@gmail.com>
2021-09-14 13:10:16 -0400

    1.0.0 (3)

--- App.tsx
+++ App.tsx
@@ -1,9 +1,24 @@
 import { NativeBaseProvider } from "native-base";
 import { MainNav } from "navigation";
-import React from "react";
+import React, { useEffect } from "react";
 import { theme } from "style";
+import * as Admob from "expo-ads-admob";
 
 export default function App() {
+  useEffect(() => {
+    (async () => {
+      try {
+        const res = await Admob.getPermissionsAsync();
+        console.log(res);
+        if (!res.granted && res.canAskAgain) {
+          await Admob.requestPermissionsAsync();
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }, []);
+
   return (
     <NativeBaseProvider theme={theme}>
       <MainNav />
--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -13,6 +13,7 @@ const Layout: React.FC = (props) => {
         <AdMobBanner
           bannerSize="smartBannerPortrait"
           adUnitID={adUnitID} // Test ID, Replace with your-admob-unit-id
+          servePersonalizedAds // true or false
           onDidFailToReceiveAdWithError={(e) => console.log(e)}
         />
       </SafeAreaView>
