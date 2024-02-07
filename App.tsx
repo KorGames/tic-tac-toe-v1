d4df4514ccde828e9f7830cab10d4c21bd9945f0
Ion Korol <ion@korol.dev>
2024-02-06 22:34:21 -0500

    Add track permission

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ import * as SplashScreen from "expo-splash-screen";
 import { View } from "react-native";
 import { Jost_400Regular, Jost_500Medium, useFonts, Jost_700Bold } from "@expo-google-fonts/jost";
 import { AsyncPromptProvider } from "hooks/useAsyncPrompt";
+import { requestTrackingPermissionsAsync } from "expo-tracking-transparency";
 
 SplashScreen.preventAutoHideAsync();
 
@@ -19,6 +20,7 @@ export default function App() {
     if (fontsLoaded || fontError) {
       await SplashScreen.hideAsync();
     }
+    await requestTrackingPermissionsAsync();
   }, [fontsLoaded, fontError]);
 
   if (!fontsLoaded && !fontError) {
